a178fc7fc376b9d23b9a71ff3df885769c3ace4b
Pranay <pranaydwivedi32@gmail.com>
2023-02-08 11:36:28 +0530

    Directory
    Components Container , Updated

---
+++ crwn-clothing-store/src/App.js
@@ -0,0 +1,12 @@
+import "./catergories.style.scss";
+import CategoryContainer from "./components/category-container/category-container.component";
+
+function App() {
+  return (
+    <div>
+      <CategoryContainer />
+    </div>
+  );
+}
+
+export default App;
